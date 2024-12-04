6fbb75058ba6c0b31b35d0cb66c1fa8ac2d139fe
miss-jane-osa <jane_i@hotmail.co.uk>
2024-12-04 16:07:56 +0000

    Updated the functionality so when the user selects a city the time shown is correct and constantly updating. Also updated the CSS styling

--- javascript/index.js
+++ javascript/index.js
@@ -21,28 +21,64 @@ function updateTime() {
       "h:mm:ss [<small>]A[</small>]"
     );
   }
+  //Moscow
+  let moscowElement = document.querySelector("#moscow");
+  if (moscowElement) {
+    let moscowDateElement = moscowElement.querySelector(".date");
+    let moscowTimeElement = moscowElement.querySelector(".time");
+    let moscowTime = moment().tz("Europe/Moscow");
+    moscowDateElement.innerHTML = moscowTime.format("MMMM Do YYYY");
+    moscowTimeElement.innerHTML = moscowTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 updateTime();
 setInterval(updateTime, 1000);
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
   }
+
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
+
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `<div class="city">
-          <div>
-            <h2>${cityName}</h2>
-            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
-          </div>
-          <div class="time">${cityTime.format(
-            "h:mm:ss"
-          )}<small>${cityTime.format("A")}</small></div>
-        </div> 
-        <a href="/">All Cities</a>`;
+  citiesElement.innerHTML = `
+    <div class="city">
+      <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      </div>
+      <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
+    "A"
+  )}</small></div>
+    </div> 
+    <a href="/">All Cities</a>`;
 }
+
+function updateNewCityTime() {
+  let citiesElement = document.querySelector("#cities .city h2");
+  if (citiesElement) {
+    let cityName = citiesElement.textContent;
+    let cityTimeZone = moment.tz
+      .names()
+      .find((zone) => zone.includes(cityName.replace(" ", "_")));
+
+    if (cityTimeZone) {
+      let cityTime = moment().tz(cityTimeZone);
+      let timeElement = document.querySelector("#cities .time");
+      timeElement.innerHTML = `${cityTime.format(
+        "h:mm:ss"
+      )}<small>${cityTime.format("A")}</small>`;
+    }
+  }
+}
+
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
+
+setInterval(updateNewCityTime, 1000);
